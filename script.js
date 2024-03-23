cb5eaa7512e8ff4fae039f55176f9089ab16692b
rayalva407 <rayalva407@gmail.com>
2024-03-23 14:31:07 -0600

    feat: allows opacity to increment on each hover

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const drawingBoard = document.querySelector('.drawing-board')
 const form = document.querySelector(".form")
+let opacity = 0;
 
 function createDivs(num1, num2, color) {
   for (let i = 0; i < (num1 * num2); i++) {
@@ -35,6 +36,7 @@ form.addEventListener("submit", (e) => {
   const field2 = document.querySelector(".num2")
   const field3 = document.querySelector("#color")
   const field4 = document.querySelector(".random")
+  const field5 = document.querySelector(".opacity")
 
   const num1 = Number(field1.value)
   const num2 = Number(field2.value)
@@ -52,10 +54,22 @@ form.addEventListener("submit", (e) => {
     createDivs(num1, num2, color)
   }
 
+  if (field5.checked) {
+    const segments = document.querySelectorAll(".drawing-board-segment")
+
+    segments.forEach((el) => {
+      el.addEventListener("mouseover", (e) => {
+        el.style.opacity = opacity;
+        opacity = opacity + 0.005;
+      })
+    })
+  }
+
   field1.value = ""
   field2.value = ""
   field3.value = "#000000"
   field4.checked = false;
+  field5.checked = false;
 })
 
 createDivs(16, 16, "black")
