99c25b20631ae8838bb5b9f7a039493de8c80003
rayalva407 <rayalva407@gmail.com>
2024-03-23 11:42:47 -0600

    feat: allows user to create a grid with specified dimensions

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-function createDivs() {
-  for (let i = 0; i < (16 * 16); i++) {
+function createDivs(num1, num2) {
+  for (let i = 0; i < (num1 * num2); i++) {
     const div = document.createElement('div')
     div.classList.add('drawing-board-segment')
+    div.addEventListener("mouseover", (e) => {
+      div.style.background = "black"
+    })
     const drawingBoard = document.querySelector('.drawing-board')
     drawingBoard.append(div)
   }
 }
 
-createDivs()
+createDivs(16, 16)
 
-segments = document.querySelectorAll(".drawing-board-segment")
+const form = document.querySelector(".form")
 
-segments.forEach(el => {
-  el.addEventListener("mouseover", (e) => {
-    el.style.background = "black"
-  })
-});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const field1 = document.querySelector(".num1")
+  const field2 = document.querySelector(".num2")
+
+  const num1 = Number(document.querySelector(".num1").value)
+  const num2 = Number(document.querySelector(".num2").value)
+  const drawingBoard = document.querySelector('.drawing-board')
+
+  while (drawingBoard.hasChildNodes()) {
+    drawingBoard.removeChild(drawingBoard.lastChild)
+  }
+  createDivs(num1, num2)
+  field1.value = ""
+  field2.value = ""
+})
+
