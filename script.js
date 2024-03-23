70b8e578831c66ec91b904fb47ad6ba8184ea6a6
rayalva407 <rayalva407@gmail.com>
2024-03-23 12:02:49 -0600

    feat: allows user to choose a color

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-function createDivs(num1, num2) {
+const drawingBoard = document.querySelector('.drawing-board')
+const form = document.querySelector(".form")
+
+function createDivs(num1, num2, color) {
   for (let i = 0; i < (num1 * num2); i++) {
     const div = document.createElement('div')
     div.classList.add('drawing-board-segment')
     div.addEventListener("mouseover", (e) => {
-      div.style.background = "black"
+      div.style.background = color
     })
     const drawingBoard = document.querySelector('.drawing-board')
     drawingBoard.append(div)
   }
 }
 
-createDivs(16, 16)
+createDivs(16, 16, "black")
 
-const form = document.querySelector(".form")
+
 
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const field1 = document.querySelector(".num1")
   const field2 = document.querySelector(".num2")
+  const field3 = document.querySelector("#color")
 
-  const num1 = Number(document.querySelector(".num1").value)
-  const num2 = Number(document.querySelector(".num2").value)
-  const drawingBoard = document.querySelector('.drawing-board')
+  const num1 = Number(field1.value)
+  const num2 = Number(field2.value)
+  const color = field3.value
 
   while (drawingBoard.hasChildNodes()) {
     drawingBoard.removeChild(drawingBoard.lastChild)
   }
-  createDivs(num1, num2)
+
+  createDivs(num1, num2, color)
+  
   field1.value = ""
   field2.value = ""
+  field3.value = "#000000"
 })
 
